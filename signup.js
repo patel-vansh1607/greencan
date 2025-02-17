6ef8b53d107578e0d4f749b907827187ec3ee2c6
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-17 20:15:45 +0300

    backend

--- signup.js
+++ signup.js
@@ -11,6 +11,7 @@ const fullName = document.getElementById("fullName")
 const email = document.getElementById("email")
 const password = document.getElementById("password")
 const confirmPassword = document.getElementById("confirmPassword")
+const signinButton = document.getElementById("signinButton")
 
 fullName.addEventListener("change", (e) =>{
     const name = e.target.value
@@ -52,3 +53,26 @@ function validateEmail(email){
     return emailRegex.test(email);
 
 }
+
+
+signinButton.addEventListener("click", (e) =>{
+    console.log(details)
+    const { fullName, email, password,  confirmPassword } = details
+    console.log(fullName, email, password, confirmPassword)
+    const isNamesValidated = validateNames (fullName)
+    const isEmailValidated = validateEmail (email)
+    const isPasswordValidated = validatePassword(password, confirmPassword)
+
+   if (isPasswordValidated && isNamesValidated && isPhoneNumberValidated && isEmailValidated){
+        window.location.href = "./main.html"
+        console.log(details)
+
+   } else {
+    alertSpan.classList.remove("alert-off-message")
+    alertSpan.classList.add("alert-on-message")
+   }
+   setTimeout(() => {
+    alertSpan.classList.remove("alert-on-message");
+    alertSpan.classList.add("alert-off-message");
+}, 5000);
+})
