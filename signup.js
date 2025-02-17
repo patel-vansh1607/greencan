6994ffec7eb089a2790c241adf7bbb7210db3cc4
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-17 20:07:32 +0300

    adding fucntions for password, names and email

--- signup.js
+++ signup.js
@@ -31,3 +31,24 @@ confirmPassword.addEventListener("change", (e) => {
     const name = e.target.value
     details.confirmPassword = name
 })
+
+function validatePassword(password,confirmPassword){
+    if (password===confirmPassword && password.length >= 4 && password!== "" && password.length != 0 && password.trim().length!==0 && /\d/.test(password) && /[a-zA-Z]/.test(password)){
+        return true;
+    } else {
+        return false;
+    }
+}
+function validateNames(fullName){
+    if (fullName.length <= 4 || !/^[A-Za-z]+$/.test(fullName|| fullName.length <=2 )){
+        return false
+    } else {
+        return true
+    }
+
+}
+function validateEmail(email){
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+
+}
