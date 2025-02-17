950e4ba574e886a736dbd44c3209314370ff7e19
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-17 20:03:21 +0300

    event listener for full name

--- signup.js
+++ signup.js
@@ -11,3 +11,8 @@ const fullName = document.getElementById("fullName")
 const email = document.getElementById("email")
 const password = document.getElementById("password")
 const confirmPassword = document.getElementById("confirmPassword")
+
+fullName.addEventListener("change", (e) =>{
+    const name = e.target.value
+    details.fullName = name
+})
