47a52220a9a05c4dbab68770d3823321b6ca8f56
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-18 10:50:17 +0300

    added scripts

--- Scripts/signup.js
+++ Scripts/signup.js
@@ -1,78 +1,11 @@
-console.log("User Details = ")
+const container = document.querySelector('.container');
+const registerBtn = document.querySelector('.register-btn');
+const loginBtn = document.querySelector('.login-btn');
 
-let details = {
-    fullName: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-}
-
-const fullName = document.getElementById("fullName")
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const confirmPassword = document.getElementById("confirmPassword")
-const signinButton = document.getElementById("signinButton")
-
-fullName.addEventListener("change", (e) =>{
-    const name = e.target.value
-    details.fullName = name
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
 })
 
-email.addEventListener("change", (e) =>{
-    const name = e.target.value
-    details.email = name
-})
-
-password.addEventListener("change", (e) =>{
-    const name = e.target.value
-    details.password = name
-})
-
-confirmPassword.addEventListener("change", (e) => {
-    const name = e.target.value
-    details.confirmPassword = name
-})
-
-function validatePassword(password,confirmPassword){
-    if (password===confirmPassword && password.length >= 4 && password!== "" && password.length != 0 && password.trim().length!==0 && /\d/.test(password) && /[a-zA-Z]/.test(password)){
-        return true;
-    } else {
-        return false;
-    }
-}
-function validateNames(fullName){
-    if (fullName.length <= 4 || !/^[A-Za-z]+$/.test(fullName|| fullName.length <=2 )){
-        return false
-    } else {
-        return true
-    }
-
-}
-function validateEmail(email){
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailRegex.test(email);
-
-}
-
-
-signinButton.addEventListener("click", (e) =>{
-    console.log(details)
-    const { fullName, email, password,  confirmPassword } = details
-    console.log(fullName, email, password, confirmPassword)
-    const isNamesValidated = validateNames (fullName)
-    const isEmailValidated = validateEmail (email)
-    const isPasswordValidated = validatePassword(password, confirmPassword)
-
-   if (isPasswordValidated && isNamesValidated && isPhoneNumberValidated && isEmailValidated){
-        window.location.href = "./main.html"
-        console.log(details)
-
-   } else {
-    alertSpan.classList.remove("alert-off-message")
-    alertSpan.classList.add("alert-on-message")
-   }
-   setTimeout(() => {
-    alertSpan.classList.remove("alert-on-message");
-    alertSpan.classList.add("alert-off-message");
-}, 5000);
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
 })
