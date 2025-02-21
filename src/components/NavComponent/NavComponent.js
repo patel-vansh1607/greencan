a76bc647f7bf3fe243d1372d8b425a6829338c1e
josephkipkemoi <jkemboe@gmail.com>
2025-02-21 09:15:09 +0000

    Services page

--- src/components/NavComponent/NavComponent.js
+++ src/components/NavComponent/NavComponent.js
@@ -1,19 +1,20 @@
+import { Link } from "react-router"
 import "./NavComponent.css"
 import MainLogo from "./green-can-main-logo.png"
 
 function NavComponent() {
     return (
         <nav className="dt-navbar navbar navbar-expand-lg navbar-light">
-              <a className="navbar-brand" href="#">
+              <Link className="navbar-brand" to="/">
                 <div className="logo"><img src={MainLogo} alt="main-logo"/></div>
-              </a>
+              </Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <a className="nav-link text-white" href="#">Services</a>
+                        <Link className="nav-link text-white" to="/services">Services</Link>
                     </li> 
                     <li className="nav-item">
                         <a className="nav-link text-white" href="#">Blog</a>
