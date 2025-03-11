97662f0a0c8c1ca493082de0654d4e9bed1cef6a
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-11 12:58:27 +0300

    docs: :rocket: deploying router dom

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AccountsPage from './pages/accounts';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App/>,
+  },
+  {
+    path: '/accounts',
+    element: <AccountsPage/>
+  }
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
