99c7517c7fc9d371b3166adfc804e15c83261683
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-11 11:44:22 +0300

    covnerting html to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const Welcome = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="leaves">
+        {[...Array(15)].map((_, index) => (
+          <i key={index}></i>
+        ))}
+      </div>
+      <div className="sec1">
+        <img src="./Images/circle-gradient.png" alt="circle-gradient" />
+        <img className="img2" src="./Images/circle-gradient.png" alt="circle-gradient" />
+      </div>
+      <div className="sec2">
+        <h4>Welcome to</h4>
+        <img className="mainlogo" src="./Images/GreenCan logo-01.png" alt="main-logo" />
+      </div>
+      <div className="sec3">
+        <div className="btn1">
+          <a href="./accounts.html">
+            <button>
+              Login / Sign Up
+              <div className="icon-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 26.3 65.33"
+                >
+                  <path
+                    className="fil0"
+                    d="M13.98 52.87c0.37,-0.8 0.6,-1.74 0.67,-2.74 1.01,1.1 2.23,2.68 1.24,3.87 ..."
+                  ></path>
+                </svg>
+              </div>
+              <div className="icon-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 11.67 37.63"
+                >
+                  <path
+                    className="fil0"
+                    d="M7.63 35.26c-0.02,0.13 0.01,0.05 -0.06,0.14 -0,0 -0.08,0.07 -0.11,0.1 ..."
+                  ></path>
+                </svg>
+              </div>
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Welcome;
