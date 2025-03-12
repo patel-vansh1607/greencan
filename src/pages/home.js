44095a4fe20c24d8f01f13b6845784ea64d9fe7d
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-12 10:03:28 +0300

    adding content to home page

---
+++ src/pages/home.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./home.css"
+
+
+const Home = () => {
+  return (
+    <div>
+      <div className="main">
+        <div className="hero">
+            <div className="overlay1"></div>
+            <div class="hero-content">
+                <h1>Welcome to Greencan</h1>
+                <p>Whether you're an individual, business, or community, GreenCan helps <br></br>you turn waste into value through innovative recycling solutions,<br></br>eco-friendly initiatives, and our Green Credit Programme. Join us in<br></br>building a cleaner, greener tomorrow—one step at a time. <br></br>Start making an impact today!</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
